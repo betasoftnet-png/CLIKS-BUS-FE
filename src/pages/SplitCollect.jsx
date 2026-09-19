83e0484469a5cfb7cb58de691935427bf4063588
btc002c <btc002c.btcglobal@gmail.com>
2026-09-19 11:05:53 +0530

    feat: implement BusinessSplitCollect and SplitCollect pages for expense sharing

--- src/pages/SplitCollect.jsx
+++ src/pages/SplitCollect.jsx
@@ -1,6 +1,6 @@
-import BusinessSplitCollect, { getDirectAttachmentUrl, resolveFileUrl, resolveAttachmentUrl, isPdfFile, isImageFile, getViewableUrl, openAttachmentInNewTab, calculateGroupOutlay } from './BusinessSplitCollect';
+import BusinessSplitCollect, { getDirectAttachmentUrl, resolveFileUrl, resolveAttachmentUrl, isPdfFile, isImageFile, getViewableUrl, openAttachmentInNewTab, calculateGroupOutlay, getEligibleExpensesForDebt } from './BusinessSplitCollect';
 
 export default BusinessSplitCollect;
-export { getDirectAttachmentUrl, resolveFileUrl, resolveAttachmentUrl, isPdfFile, isImageFile, getViewableUrl, openAttachmentInNewTab, calculateGroupOutlay };
+export { getDirectAttachmentUrl, resolveFileUrl, resolveAttachmentUrl, isPdfFile, isImageFile, getViewableUrl, openAttachmentInNewTab, calculateGroupOutlay, getEligibleExpensesForDebt };
 
 
