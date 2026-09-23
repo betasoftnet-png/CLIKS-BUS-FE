95ee1f779289151d77cd10292b432e3f72c7dcca
btc002c <btc002c.btcglobal@gmail.com>
2026-09-23 10:09:12 +0530

    feat: add BusinessSplitCollect and SplitCollect pages for expense splitting and payments

---
+++ src/pages/payments/SplitCollect.jsx
@@ -0,0 +1,30 @@
+import BusinessSplitCollect, { 
+    getDirectAttachmentUrl, 
+    resolveFileUrl, 
+    resolveAttachmentUrl, 
+    isPdfFile, 
+    isImageFile, 
+    getViewableUrl, 
+    openAttachmentInNewTab, 
+    calculateGroupOutlay, 
+    getEligibleExpensesForDebt, 
+    isPrimaryExpense, 
+    isSettlementLinkedToExpense, 
+    calculateSessionBalances 
+} from '../BusinessSplitCollect';
+
+export default BusinessSplitCollect;
+export { 
+    getDirectAttachmentUrl, 
+    resolveFileUrl, 
+    resolveAttachmentUrl, 
+    isPdfFile, 
+    isImageFile, 
+    getViewableUrl, 
+    openAttachmentInNewTab, 
+    calculateGroupOutlay, 
+    getEligibleExpensesForDebt, 
+    isPrimaryExpense, 
+    isSettlementLinkedToExpense, 
+    calculateSessionBalances 
+};
